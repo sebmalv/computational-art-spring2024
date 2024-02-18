8c33f913b055f0f03be70ab4d0b93d7183a0944e
sebmalv <sebmalvarenga@gmail.com>
2024-02-18 16:51:31 -0500

    j

--- assignments/03_physics/sketch.js
+++ assignments/03_physics/sketch.js
@@ -20,7 +20,7 @@ class Animal {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = random(10, 30);
+    this.radius = random(20, 40);
     this.speed = random(1, 3);
     this.color = color(random(255), random(255), random(255));
   }
@@ -37,5 +37,10 @@ class Animal {
   display() {
     fill(this.color);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    // Draw a cute face
+    fill(255);
+    ellipse(this.x - this.radius * 0.2, this.y - this.radius * 0.2, this.radius * 0.5, this.radius * 0.5);
+    ellipse(this.x + this.radius * 0.2, this.y - this.radius * 0.2, this.radius * 0.5, this.radius * 0.5);
+    arc(this.x, this.y + this.radius * 0.1, this.radius * 1, this.radius * 0.8, 0, PI, CHORD);
   }
 }
